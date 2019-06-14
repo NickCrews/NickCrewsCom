45069be8e481ce45eda41c1a7ace7c2e8fb3ab88
NickCrews <nicholas.b.crews@gmail.com>
2019-06-14 08:54:47 -0600

    put photos in divs and links

--- build/js/photos.js
+++ build/js/photos.js
@@ -1,15 +1,25 @@
 // @photo: object with src, title, description fields
 function addPhoto(photo) {
-  $('.home').prepend($('<img>', {
-    id: photo.src,
-    src: 'resources/images/photos/' + photo.src,
-    alt: photo.src,
-  }));
+  $('.photo-grid').prepend(
+    $('<div>', {
+      class: 'photo-grid-item'
+    }).prepend(
+      $('<a>', {
+        href: 'resources/images/photos/' + photo.src,
+      }).prepend(
+        $('<img>', {
+          id: photo.src,
+          src: 'resources/images/photos/' + photo.src,
+          alt: photo.src,
+        })
+      )
+    )
+  )
 }
 
 function loadPhotos(photoJsonPath) {
   let photo_data_loading = fetch(photoJsonPath).
-    then(response => response.json());
+  then(response => response.json());
 
   photo_data_loading.then(function(photo_data) {
     photo_data.forEach(photo => {
